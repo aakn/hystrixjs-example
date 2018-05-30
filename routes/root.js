806cb24441579bb1776dd1150277261be807705e
Ali Asgar <aliasgar@outlook.com>
2018-05-30 19:39:36 +1000

    trying fallback

--- routes/root.js
+++ routes/root.js
@@ -1,17 +1,36 @@
 import request from 'request-promise-native';
+import {
+	commandFactory as CommandFactory
+} from 'hystrixjs';
 
 export default (req, res) => {
 
-	const promise = request.get({
-		uri: 'https://jsonplaceholder.typicode.com/users/1',
+	const promise = (id) => request.get({
+		uri: `https://jsonplaceholder.typicode.com/users/${id}`,
 		json: true,
 	});
 
-	promise.then(user => {
-		return res.json({
-			user,
-		});
-	}).catch(err => {
-		return res.status(500).send(err);
+	const fallback = (err, args) => ({
+		oops: "from fallback",
+		err,
+		args,
 	});
+
+	const cmd = CommandFactory.getOrCreate('sample')
+		.run(promise)
+		.timeout(1000)
+		.fallbackTo(fallback)
+		.build();
+
+	cmd.execute(1)
+		.then(user => {
+			return res.json({
+				user,
+			});
+		}).catch(err => {
+			return res.status(500).send({
+				err,
+				oops: "direct error",
+			});
+		});
 };
